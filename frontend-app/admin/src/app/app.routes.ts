a49af1457c7d5f48d91b1ab7012fa3f25d332ecf
macelin00 <macelino@mymail.mapua.edu.ph>
2025-10-11 02:17:42 +0800

    Added Admin Inventory Page.

--- frontend-app/admin/src/app/app.routes.ts
+++ frontend-app/admin/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { DashboardComponent } from './pages/dashboard/dashboard';
+import { Inventory } from './pages/inventory/inventory';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+    { path: 'inventory', component: Inventory },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
