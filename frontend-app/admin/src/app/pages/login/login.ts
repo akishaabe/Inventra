3b6fad967a95fe2027d7e6007be69294514ea4a9
macelin00 <macelino@mymail.mapua.edu.ph>
2025-10-10 19:09:58 +0800

    Updated Login Page design
    Co-authored-by: Akisha Abesamis <akdabe05@users.noreply.github.com>

---
+++ frontend-app/admin/src/app/pages/login/login.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class LoginComponent {
+  email = '';       // ✅ changed from username
+  password = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.email === 'admin@gmail.com' && this.password === '1234') {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
+}
