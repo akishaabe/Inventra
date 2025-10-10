618eb2237ab95b4483c324e7cfe93de2a6b7208b
macelin00 <macelino@mymail.mapua.edu.ph>
2025-10-11 02:42:40 +0800

    Admin: Updated Side Bar and Inventory Page.

--- frontend-app/admin/src/app/components/sidebar/sidebar.ts
+++ frontend-app/admin/src/app/components/sidebar/sidebar.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
+  imports: [RouterModule], // ← ADD THIS LINE
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css']
 })
@@ -10,9 +12,6 @@ export class SidebarComponent {
   @Input() isOpen = false;
 
   logout() {
-    // Example logout behavior
     console.log('Logging out...');
-    // navigate to login if you have router
-    // this.router.navigate(['/login']);
   }
 }
