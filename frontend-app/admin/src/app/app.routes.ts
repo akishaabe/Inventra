ec73ebc8004dc045b9e27b22f31d08bba889a009
macelin00 <macelino@mymail.mapua.edu.ph>
2025-10-11 16:32:55 +0800

    Integrated Login to Admin Role. Added Forecasting Design Page.

--- frontend-app/admin/src/app/app.routes.ts
+++ frontend-app/admin/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 import { LoginComponent } from './pages/login/login';
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { Inventory } from './pages/inventory/inventory';
+import { Forecasting } from './pages/forecasting/forecasting';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-    { path: 'inventory', component: Inventory },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'inventory', component: Inventory },
+  { path: 'forecasting', component: Forecasting },
+  { path: '**', redirectTo: '' }
 ];
--- frontend-app/admin/src/app/pages/login/login.ts
+++ frontend-app/admin/src/app/pages/login/login.ts
@@ -1,25 +1,45 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { GoogleAuthProvider, signInWithPopup, Auth } from '@angular/fire/auth';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class LoginComponent {
-  email = '';  
-  password = '';
+  email: string = '';
+  password: string = '';
+
+  private auth = inject(Auth);
 
   constructor(private router: Router) {}
 
-  login() {
-    if (this.email === 'admin@gmail.com' && this.password === '1234') {
-      this.router.navigate(['/home']);
-    } else {
-      alert('Invalid credentials');
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    this.router.navigate(['/2fa']);
+  }
+
+  async loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(this.auth, provider);
+      console.log('Google user:', result.user);
+      this.router.navigate(['/dashboard']);
+    } catch (error) {
+      console.error('Google Sign-In failed:', error);
     }
+  }
+
+  goBack() {
+    this.router.navigate(['/home']);
   }
 }
