6c5d6dd0e6fef91f4397087168c36192db3a9993
macelin00 <macelino@mymail.mapua.edu.ph>
2025-10-12 20:31:24 +0800

    Fixed Frontend

--- frontend-app/admin/src/app/components/sidebar/sidebar.ts
+++ frontend-app/admin/src/app/components/sidebar/sidebar.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css']
 })
-export class Sidebar {
+export class SidebarComponent {
   @Input() isOpen = false;
 
   constructor(private router: Router) {}
